c05f8ed28e847f216ba8455f9a0c971354323c1f
Fauzi0309 <fauzirahmandhani@gmail.com>
2024-08-08 18:02:44 +0700

    feat: add Album

---
+++ src/server.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const Hapi = require('@hapi/hapi');
+const albums = require('./api/albums');
+const AlbumService = require('./services/postgres/AlbumService');
+const AlbumsValidator = require('./validator/albums');
+
+const init = async () => {
+    const albumService = new AlbumService();
+    const server = Hapi.server({
+      port: process.env.PORT,
+      host: process.env.HOST,
+      routes: {
+        cors: {
+          origin: ['*'],
+        },
+      },
+    });
+   
+    await server.register({
+      plugin: albums,
+      options: {
+        service: albumService,
+        validator: AlbumsValidator,
+      },
+    });
+
+    await server.start();
+    console.log(`Server berjalan pada ${server.info.uri}`);
+}
+
+init();
