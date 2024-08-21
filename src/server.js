cf3b6a013dcf86b805d787086f1128c63f852727
Fauzi0309 <fauzirahmandhani@gmail.com>
2024-08-21 14:01:45 +0700

    user registration implemented

--- src/server.js
+++ src/server.js
@@ -1,17 +1,25 @@
 require("dotenv").config();
 
 const Hapi = require("@hapi/hapi");
+const ClientError = require("./exceptions/ClientError");
+
 const albums = require("./api/albums");
 const AlbumService = require("./services/postgres/AlbumService");
 const AlbumsValidator = require("./validator/albums");
-const ClientError = require("./exceptions/ClientError");
+
 const SongService = require("./services/postgres/SongService");
 const songs = require("./api/songs");
 const SongsValidator = require("./validator/songs");
 
+const UsersService = require("./services/postgres/UsersService");
+const users = require("./api/users");
+const UsersValidator = require("./validator/users");
+
 const init = async () => {
   const albumService = new AlbumService();
   const songsService = new SongService();
+  const usersService = new UsersService();
+
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -23,22 +31,29 @@ const init = async () => {
   });
 
   // Registering albums plugin
-  await server.register({
+  await server.register([
+    {
     plugin: albums,
     options: {
       service: albumService,
       validator: AlbumsValidator,
     },
-  });
-
-  // Registering songs plugin
-  await server.register({
+  },
+  {
     plugin: songs,
     options: {
       service: songsService,
       validator: SongsValidator,
     },
-  });
+  },
+  {
+    plugin: users,
+    options: {
+      service: usersService,
+      validator: UsersValidator,
+    },
+  },
+]);
 
   server.ext("onPreResponse", (request, h) => {
     // mendapatkan konteks response dari request
